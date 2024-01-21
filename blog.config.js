fac99d7c52ffc6560518c9d0df93469af63555fa
Tyler-Bastian <157269603+Tyler-Bastian@users.noreply.github.com>
2024-01-21 21:47:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -55,7 +55,8 @@ const BLOG = {
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
-    '"PingFang SC"',
+    //'"PingFang SC"',
+    'roboto',
     '-apple-system',
     'BlinkMacSystemFont',
     '"Hiragino Sans GB"',
